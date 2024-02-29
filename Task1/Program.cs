e0a23cefd979e66448d5205d87aceb406aeb1acb
Ozymadias11 <natchkebiadima1@gmail.com>
2024-02-29 22:14:03 +0400

    added solution for task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,6 +10,50 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+
+            //some test Cases
+            Console.WriteLine(isPalindrome("aba") ? "YES" : "NO"); //YES
+            Console.WriteLine(isPalindrome("madam") ? "YES" : "NO"); //YES
+            Console.WriteLine(isPalindrome("A man, a plan, a canal, Panama!") ? "YES" : "NO"); //NO
+            Console.WriteLine(isPalindrome("Is the string a palindrome?") ? "YES" : "NO"); //NO
+        }
+
+
+
+        static bool isPalindrome(string input)
+        {
+            string cleanedInput = removeSpecialChars(input);
+            cleanedInput = cleanedInput.ToLower();
+            return isPalindromeChecker(cleanedInput);
+        }
+
+        // for example: if we are dealing with strings that end with "?", "!" 
+        //we should remove that first and then check
+        static string removeSpecialChars(string s)
+        {
+            char[] specialChars = { '!', '.', '?' };
+            foreach(char c in specialChars)
+            {
+                s = s.Replace(c.ToString(), string.Empty);
+            }
+            return s;
+        }
+        static bool isPalindromeChecker(string input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+
+            while(left < right) 
+            {
+                if (input[left] != input[right]) 
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+            return true;
         }
     }
 }
